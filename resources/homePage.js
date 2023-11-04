05363feb8208333a67fa24939fcbab86667e76cd
Phurgoph <hargrh@rpi.edu>
2023-11-04 19:26:28 -0400

    home page

--- resources/homePage.js
+++ resources/homePage.js
@@ -2,3 +2,18 @@ function checkCode(formObj) {
 
 
 };
+
+
+function clear(){
+   if (document.getElementById("comment").innerHTML == "XXXX-XXXX-XX"){
+      document.getElementById("comment").innerHTML = "";
+   }
+}
+
+function populate(){
+
+   if (document.getElementById("comment").innerHTML == ""){
+      document.getElementById("comment").innerHTML = "XXXX-XXXX-XX";
+   }
+
+}
